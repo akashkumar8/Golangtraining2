bc9919e80b8c6bd5a1a3f90cab006d79d9e57baf
Akash Kumar <67784046+akashkumar8@users.noreply.github.com>
2020-11-05 18:04:14 +0530

    Select Method

--- select.go
+++ select.go
@@ -5,23 +5,24 @@ import (
     "time"
 )
 
-func process(ch chan string) {  
-    time.Sleep(10500 * time.Millisecond)
-    ch <- "process successful"
+func server1(ch chan string) {  
+    time.Sleep(6 * time.Second)
+    ch <- "from server1"
 }
+func server2(ch chan string) {  
+    time.Sleep(3 * time.Second)
+    ch <- "from server2"
 
+}
 func main() {  
-    ch := make(chan string)
-    go process(ch)
-    for {
-        time.Sleep(1000 * time.Millisecond)
-        select {
-        case v := <-ch:
-            fmt.Println("received value: ", v)
-            return
-        default:
-            fmt.Println("Oops! No any value received")
-        }
+    output1 := make(chan string)
+    output2 := make(chan string)
+    go server1(output1)
+    go server2(output2)
+    select {
+    case s1 := <-output1:
+        fmt.Println(s1)
+    case s2 := <-output2:
+        fmt.Println(s2)
     }
-
 }
