ad556820c91998b4548a3b960c03fc6db720f3b2
Akash Kumar <67784046+akashkumar8@users.noreply.github.com>
2020-11-16 18:28:37 +0530

    Switch statement

---
+++ switchingds.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	i := 2
+	switch i {
+	case 1:
+		fmt.Println("heyy")
+	case 2:
+		fmt.Println("folks")
+	case 3:
+
+		fmt.Println("kloud")
+	default:
+		fmt.Println("something else")
+	}
+}
